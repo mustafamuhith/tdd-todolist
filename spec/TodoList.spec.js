2d6291197985e7a40a6ad0facff6f772c13eaa03
Nathan King <nathanking3590@gmail.com>
2022-11-01 08:59:56 +0000

    Update TodoList.spec.js

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -1,17 +1,1 @@
-const TodoList = require("../src/TodoList.js")
 
-describe("TodoList", () => {
-  it("creates a todo item", () => {
-    // set up
-    const todoList = new TodoList()
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
-    // execute
-    const result = todoList.create("turn the heating on!")
-    // verify
-    expect(result).toEqual(expected)
-  })
-})
